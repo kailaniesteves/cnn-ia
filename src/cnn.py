25bb3ddabebce7ebd237a63b508b85075e1d6253
Kailani Esteves <kailaniesteves@gmail.com>
2023-06-27 09:58:30 -0300

    feat: Adding test feature and loss plot

--- src/cnn.py
+++ src/cnn.py
@@ -28,13 +28,36 @@ model.compile(optimizer='adam',
 history = model.fit(train_images, train_labels, epochs=10, 
                     validation_data=(test_images, test_labels))
 
-plt.plot(history.history['accuracy'], label='accuracy')
-plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.ylim([0.5, 1])
-plt.legend(loc='lower right')
+plt.figure()
+plt.ylabel("Loss (training and validation)")
+plt.xlabel("Training Steps")
+plt.ylim([0,2])
+plt.plot(history.history["loss"])
+plt.plot(history.history["val_loss"])
 plt.show()
+
+plt.figure()
+plt.ylabel("Accuracy (training and validation)")
+plt.xlabel("Training Steps")
+plt.ylim([0,1])
+plt.plot(history.history["accuracy"])
+plt.plot(history.history["val_accuracy"])
+plt.show()
+
+# Testing
+
+classNames = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+x,y = (test_images[10], test_labels[10])
+plt.imshow(x)
+plt.axis('off')
+plt.show()
+
+# Expand the validation image to (1, 224, 224, 3) before predicting the label
+prediction_scores = model.predict(np.expand_dims(x, axis=0))
+predicted_index = np.argmax(prediction_scores)
+print("True label: " + classNames[int(y)])
+print("Predicted label: " + classNames[predicted_index])
 
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 print(test_acc)
